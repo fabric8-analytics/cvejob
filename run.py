c57ff13dfde62f47768678ff457c05f50bfc73b8
Marek Cermak <macermak@redhat.com>
2018-10-05 14:09:13 +0200

    [WIP] Allow cvejob to be ran on specific date range
    - Introduce CVEJOB_DATE_RANGE environment variable
    
    enhancements:
    - cvejob now inferes and fetches relevant feeds without user intervention
    - simple optimization to handle simple cases (like CVE out of feed range)
    
    - improve and unify logging formatting with nvdlib
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   cvejob/config.py
    modified:   cvejob/utils.py
    modified:   run.py

--- run.py
+++ run.py
@@ -3,21 +3,28 @@
 import os
 import re
 
-import logging
-
 import multiprocessing
 
+import nvdlib
 from nvdlib.manager import FeedManager
+from nvdlib.query_selectors import in_range
 
 from cvejob.filters.input import validate_cve
 from cvejob.config import Config
 from cvejob.identifiers import get_identifier
 from cvejob.selectors.basic import VersionExistsSelector
 from cvejob.outputs.victims import VictimsYamlOutput
+from cvejob.utils import parse_date_range
+
+import logging
 
 
-logging.basicConfig(level=logging.DEBUG)
+# logging configuration
+logging.basicConfig(level=logging.DEBUG,
+                    handlers=[nvdlib.get_logging_handler()])  # use nvdlib's handler
+
 logger = logging.getLogger('cvejob')
+
 
 FEED_NAME_PATTERN = r"nvdcve-" \
                     r"(?P<version>[\d.]+)-" \
@@ -29,17 +36,50 @@ def run():
     """Run CVEjob."""
     feed_dir = Config.feed_dir
     feed_names = Config.feed_names
+    date_range = Config.date_range
+
+    cherrypicked_cve_id = Config.cve_id
+    cherrypicked_year = None
+
+    if cherrypicked_cve_id:
+        cherrypicked_year = cherrypicked_cve_id.split(sep='-')[1]
+
+    if date_range:
+        date_range = parse_date_range(Config.date_range)
+
+        feed_names = range(date_range[0].year, date_range[1].year + 1)
+
+        if cherrypicked_cve_id:  # optimization check
+
+            if int(cherrypicked_year) not in feed_names:
+                logger.info(
+                    "[{picked_cve_id}] does not belong to the given feed range:"
+                    " {date_range}".format(
+                        picked_cve_id=cherrypicked_cve_id,
+                        date_range=date_range
+                    ))
+
+                return
 
     if not feed_names:
-        # load all feeds in the feed_dir
 
-        feed_names = [
-            f_name for f_name in os.listdir(feed_dir)
-            if re.fullmatch(FEED_NAME_PATTERN, f_name, re.IGNORECASE)
-        ]
+        if cherrypicked_cve_id:
+            feed_names = [cherrypicked_year]
 
-        if not feed_names:
-            logger.error("Feeds have not been found in {}".format(feed_dir))
+        else:  # load all feeds in the feed_dir
+            feed_names = [
+                f_name for f_name in os.listdir(feed_dir)
+                if re.fullmatch(FEED_NAME_PATTERN, f_name, re.IGNORECASE)
+            ]
+
+        if not feed_names:  # if there are still no feed present -> default feed
+            logger.info(
+                ("No feeds have been selected or found in {f_dir}.\n"
+                 "Default feed will be fetched: {default_feed}"
+                 ).format(
+                    f_dir=feed_dir,
+                    default_dir=FeedManager.DEFAULT_FEED_NAME
+                ))
 
     with FeedManager(n_workers=multiprocessing.cpu_count()) as feed_manager:
 
@@ -48,28 +88,38 @@ def run():
         )
         collection = feed_manager.collect(feeds)
 
-        cherrypicked_cve_id = Config.cve_id
+        if date_range:
+            collection_size_before = collection.count()
+
+            collection = collection.find(
+                {'published_date': in_range(*date_range)}
+            )
+
+            logger.debug(("Filtered out {} Documents that do not fall "
+                         "in the given range.").format(
+                collection_size_before - collection.count()
+            ))
 
         if cherrypicked_cve_id:
+
             logger.debug("Cherry-picked CVE `{cve_id}`".format(
                 cve_id=cherrypicked_cve_id
             ))
             collection = collection.find(
                 {'cve.id_': cherrypicked_cve_id}
             )
-            logger.debug("Number of CVE Documents in the collection: {}".format(
-                collection.count()
-            ))
 
-            if not collection:  # collection is empty
-                logger.info(
-                    "[{picked_cve_id}] was not found in the collection of feeds:"
-                    "{feed_names}".format(
-                        picked_cve_id=cherrypicked_cve_id,
-                        feed_names=feed_names
-                    ))
+        if not collection:  # collection is empty
+            logger.info(
+                "Collection is empty.".format(
+                    picked_cve_id=cherrypicked_cve_id,
+                ))
 
-                return
+            return
+
+        logger.debug("Number of CVE Documents in the collection: {}".format(
+            collection.count()
+        ))
 
         for doc in collection:
 
@@ -93,8 +143,6 @@ def run():
                             cve_id=cve_id
                         ))
                     continue
-
-                print('Candidates:', candidates)
 
                 selector = VersionExistsSelector(doc, candidates)
                 winner = selector.pick_winner()
