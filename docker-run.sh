f5c4512afc743ca556db500d5c20bfb76cd1ffe0
Marek Cermak <macermak@redhat.com>
2018-10-05 14:32:59 +0200

    Remove mounted volume from docker-run script.
    - development leftover
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-docker run -ti --rm -v $(pwd):/cvejob/:z -v $(pwd)/nvdlib/nvdlib:/usr/lib/python3.6/site-packages/nvdlib $@ fabric8-analytics/cvejob python3.6 run.py
+docker run -ti --rm -v $(pwd):/cvejob/:z $@ fabric8-analytics/cvejob python3.6 run.py
