5fbef9f6d1ef09267a9cf2457b83a4566c8b73aa
Michal Srb <michal@redhat.com>
2019-03-04 12:21:31 +0100

    Introduce cpe2pkg module

---
+++ cvejob/cpe2pkg.py
@@ -0,0 +1,104 @@
+"""Wrapper around cpe2pkg tool and various helper structures/functions."""
+
+from cvejob.config import Config
+
+from decimal import Decimal
+import logging
+import subprocess
+from pathlib import Path
+
+logger = logging.getLogger('cvejob')
+
+
+class PackageNameCandidate(object):
+    """Package name candidate, with associated confidence score."""
+
+    def __init__(self, package, score):
+        """Instantiate PackageNameCandidate().
+
+        :param package: str, package name
+        :param score: decimal.Decimal, confidence score
+        """
+        if not package or not isinstance(package, str):
+            raise ValueError('"package" must be a non-empty string')
+        if not isinstance(score, Decimal):
+            raise ValueError('"score" must be a Decimal')
+
+        self.package = package
+        self.score = score
+
+    def __repr__(self):
+        return 'PackageNameCandidate({p}, {s})'.format(p=self.package, s=self.score)
+
+    def __eq__(self, other):
+        return self.score == other.score
+
+    def __lt__(self, other):
+        return self.score < other.score
+
+    def __le__(self, other):
+        return self.score <= other.score
+
+    @classmethod
+    def from_cpe2pkg_output(cls, output_line, ecosystem):
+        """Build PackageNameCandidate from the output of cpe2pkg.
+
+        :param output_line: str, single line of the cpe2pkg output
+        :param ecosystem: str, ecosystem name
+        """
+        score_str, package = output_line.split()
+        score = Decimal(score_str)
+
+        # ecosystems other than "java" don't have group IDs
+        if ecosystem != 'java':
+            package = package[len(ecosystem) + 1:]  # strip "ecosystem:"
+
+        return cls(package, score)
+
+
+def get_pkgfile_path(pkgfile_dir, ecosystem):
+    """Get path to pkgfile for given ecosystem."""
+    return str(Path(pkgfile_dir) / Path('{e}-packages'.format(e=ecosystem)))
+
+
+def run_cpe2pkg(query_str, pkgfile_path, cpe2pkg_path=Config.cpe2pkg_path):
+    """Run cpe2pkg tool with given query string.
+
+    The tool returns a list of package name candidates, with confidence score.
+
+    :param query_str: str, query string for cpe2pkg
+    :param pkgfile_path: path to a file containing package names;
+        this is a set of all possible package names (i.e. all package names in given ecosystem)
+    :param cpe2pkg_path: path to a cpe2pkg jar file
+    :return: list[str], list of lines returned from cpe2pkg;
+        example: ['10.0 package-a', '9.0 package-b']
+    """
+    logger.info('Running cpe2pkg with query: {q}'.format(q=query_str))
+
+    # run cpe2pkg tool with given query
+    cpe2pkg_output = subprocess.check_output(
+        'java -jar {cpe2pkg_path} --pkgfile {pkgfile} "{query}"'.format(
+            cpe2pkg_path=cpe2pkg_path,
+            pkgfile=pkgfile_path,
+            query=query_str
+        ),
+        shell=True,
+        universal_newlines=True
+    )
+
+    # split on lines and exclude empty lines
+    return [x for x in cpe2pkg_output.split('\n') if x]
+
+
+def build_cpe2pkg_query(vendor, product):
+    """Build a query string for cpe2pkg tool.
+
+    :param vendor: list[str], list of possible vendor strings
+    :param product: list[str], list of possible product strings
+    :return: str, query string for cpe2pkg
+    """
+    query_template = 'vendor:( {vendor} ) AND product:( {product} )'
+    # query string cannot contain colons, replace them with spaces
+    p = ' '.join(product).replace(':', ' ')
+    v = ' '.join(vendor).replace(':', ' ')
+    return query_template.format(vendor=v, product=p)
---
+++ tests/test_cpe2pkg.py
@@ -0,0 +1,80 @@
+"""Tests for cvejob.cpe2pkg module."""
+
+from cvejob.cpe2pkg import PackageNameCandidate, get_pkgfile_path, build_cpe2pkg_query, run_cpe2pkg
+from decimal import Decimal
+
+import pytest
+from pathlib import Path
+
+
+def test_package_name_candidate():
+    """Test PackageNameCandidate()."""
+    c1 = PackageNameCandidate.from_cpe2pkg_output('10.0 python:package-a', 'python')
+
+    assert c1.package == 'package-a'
+    assert c1.score == Decimal('10.0')
+    assert str(c1) == 'PackageNameCandidate(package-a, 10.0)'
+    assert c1 == c1
+
+    c2 = PackageNameCandidate.from_cpe2pkg_output('9.0 python:package-b', 'python')
+    assert c1 > c2
+    assert c2 < c1
+    assert c1 != c2
+
+    c3 = PackageNameCandidate.from_cpe2pkg_output('9.0 gid:package-c', 'java')
+    assert c3.package == 'gid:package-c'
+
+    # with invalid input
+    with pytest.raises(ValueError):
+        PackageNameCandidate(None, Decimal('1.0'))
+    with pytest.raises(ValueError):
+        PackageNameCandidate('', Decimal('1.0'))
+    with pytest.raises(ValueError):
+        PackageNameCandidate('package-a', None)
+
+
+@pytest.mark.parametrize('pkgfile_dir,ecosystem,expected', [
+    ('/pkgfile/dir/', 'java', '/pkgfile/dir/java-packages'),
+    ('/pkgfile/dir', 'python', '/pkgfile/dir/python-packages'),
+    ('/pkgfile/dir/', 'xyz', '/pkgfile/dir/xyz-packages'),
+])
+def test_get_pkgfile_path(pkgfile_dir, ecosystem, expected):
+    """Test get_pkgfile_path()."""
+    assert get_pkgfile_path(pkgfile_dir, ecosystem) == expected
+
+
+@pytest.mark.parametrize('vendor,product,query', [
+    (
+        ['apache', 'foundation'], ['tomcat', 'server'],
+        'vendor:( apache foundation ) AND product:( tomcat server )'
+    ),
+    (
+        ['apache', 'foundation'], ['apache::tomcat', 'server'],
+        'vendor:( apache foundation ) AND product:( apache  tomcat server )'
+    )
+])
+def test_build_cpe2pkg_query(vendor, product, query):
+    """Test build_cpe2pkg_query()."""
+    assert build_cpe2pkg_query(vendor, product) == query
+
+
+def test_run_cpe2pkg(mocker):
+    """Test run_cpe2pkg().
+
+    We are not really running the tool here, just mocking it.
+    """
+    mock_output = """
+1.1375153 io.vertx:vertx-core
+0.13637602 io.vertx:vertx-auth
+0.13637602 io.vertx:vertx-codegen
+0.13637602 io.vertx:vertx-codetrans
+"""
+    mock = mocker.patch('cvejob.cpe2pkg.subprocess.check_output')
+    mock.return_value = mock_output
+    pkgfile_path = Path(__file__).parent / Path('data/java-pkgfile')
+    results = run_cpe2pkg(
+        'vendor:( vertx ) AND product:( vertx io core framework )', pkgfile_path
+    )
+    assert len(results) == 4
+    assert results[0] == '1.1375153 io.vertx:vertx-core'
+    assert all(results)
