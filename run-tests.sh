c7ccbc272d6479c4d44abee3a18161e7fc00c968
Michal Srb <michal@redhat.com>
2019-03-06 08:01:00 +0100

    Refactoring in run-tests.sh
    Make sure we use the same interpreter everywhere in tests.

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+set -e
+set -x
+
 COVERAGE_THRESHOLD=40
 
 TERM=${TERM:-xterm}
@@ -10,43 +13,48 @@ RED=$(tput bold && tput setaf 1)
 GREEN=$(tput bold && tput setaf 2)
 YELLOW=$(tput bold && tput setaf 3)
 
+function find_python() {
+    # we want tests to run on python3.6
+    printf 'checking alias `python3.6` ... ' >&2
+    PYTHON=$(which python3.6 2> /dev/null)
+    if [ "$?" -ne "0" ]; then
+        printf "%sNOT FOUND%s\n" "${YELLOW}" "${NORMAL}" >&2
+
+        printf 'checking alias `python3` ... ' >&2
+        PYTHON=$(which python3 2> /dev/null)
+
+        let ec=$?
+        [ "$ec" -ne "0" ] && printf "${RED} NOT FOUND ${NORMAL}\n" && return $ec
+    fi
+
+    printf "%sOK%s\n" "${GREEN}" "${NORMAL}" >&2
+
+}
+# make sure that PYTHON environment variable is populated
+find_python
+
 function prepare_venv() {
-	# we want tests to run on python3.6
-	printf 'checking alias `python3.6` ... ' >&2
-	PYTHON=$(which python3.6 2> /dev/null)
-	if [ "$?" -ne "0" ]; then
-		printf "%sNOT FOUND%s\n" "${YELLOW}" "${NORMAL}" >&2
-
-		printf 'checking alias `python3` ... ' >&2
-		PYTHON=$(which python3 2> /dev/null)
-
-		let ec=$?
-		[ "$ec" -ne "0" ] && printf "${RED} NOT FOUND ${NORMAL}\n" && return $ec
-	fi
-
-	printf "%sOK%s\n" "${GREEN}" "${NORMAL}" >&2
-
-	${PYTHON} -m venv "venv" && source venv/bin/activate
+    ${PYTHON} -m venv "venv" && source venv/bin/activate
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
 
 
 # install the project
-pip install -r requirements.txt
+${PYTHON} -m pip install -r requirements.txt
 
 # install test dependencies
-pip install -r tests/requirements.txt
+${PYTHON} -m pip install -r tests/requirements.txt
 
 # download nltk data
-python -c "import nltk; nltk.download('words')"
-python -c "import nltk; nltk.download('punkt')"
-python -c "import nltk; nltk.download('stopwords')"
+${PYTHON} -c "import nltk; nltk.download('words')"
+${PYTHON} -c "import nltk; nltk.download('punkt')"
+${PYTHON} -c "import nltk; nltk.download('stopwords')"
 
 # ensure pytest and coverage is available
-pip install pytest pytest-cov
+${PYTHON} -m pip install pytest pytest-cov
 
 # run tests
-pytest --cov="cvejob/" --cov-report term-missing --cov-fail-under=$COVERAGE_THRESHOLD -vv tests/ $@
+${PYTHON} -m pytest --cov="cvejob/" --cov-report term-missing --cov-fail-under=$COVERAGE_THRESHOLD -vv tests/ $@
 
 `which codecov` --token=e27b6fe8-371b-41d9-9894-8d32af762c19
