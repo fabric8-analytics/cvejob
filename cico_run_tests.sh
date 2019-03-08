dde752d63369e91cd6df9b0687f124d4307d8d30
Michal Srb <michal@redhat.com>
2019-03-08 15:53:07 +0100

    [test-only] Build and push PR image

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -7,4 +7,12 @@ here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 . ${here}/cico_setup.sh
 
 make -f ${here}/Makefile build-cpe2pkg
-make -f ${here}/Makefile test
+#make -f ${here}/Makefile test
+
+yum -y install docker
+systemctl start docker
+
+
+build_image
+push_image
+
