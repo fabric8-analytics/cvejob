ac7cee62ac5b3731f677a6d0b744773caee022e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-28 11:47:51 +0100

    Check for basic attributes

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,7 +12,7 @@ def test_default_config_constructor():
 
 
 def test_default_config_attributes():
-    """Basic test for the class DefaultConfig."""
+    """Check the attributes existence for a class DefaultConfig."""
     config = DefaultConfig()
 
     # basic configuration check
@@ -21,6 +21,32 @@ def test_default_config_attributes():
                   "nvdtoolkit_export_dir")
     for attribute in attributes:
         assert hasattr(config, attribute)
+
+
+def test_default_config_attribute_values_nil():
+    """Check the attributes that needs to be set to nil (None)."""
+    config = DefaultConfig()
+
+    # the following attributes needs to be set to nil
+    assert config.feed_names is None
+    assert config.date_range is None
+    assert config.cve_id is None
+    assert config.package_name is None
+    assert config.feed_names is None
+
+
+def test_default_config_attribute_values_not_nil():
+    """Check the attributes that needs not to be set to nil (None)."""
+    config = DefaultConfig()
+
+    # the following attributes need not be set to nil
+    assert config.ecosystem is not None
+    assert config.cve_age is not None
+    assert config.feed_dir is not None
+    assert config.cpe2pkg_path is not None
+    assert config.pkgfile_dir is not None
+    assert config.use_nvdtoolkit is not None
+    assert config.nvdtoolkit_export_dir is not None
 
 
 def test_runtime_config():
