4ad3f0d2873f180f1502a47bc3aa183ad711f3ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-28 11:42:56 +0100

    Check for attributes existence for a class DefaultConfig

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,10 +5,22 @@
 from cvejob.config import DefaultConfig, RuntimeConfig
 
 
-def test_default_config():
+def test_default_config_constructor():
     """Basic test for the class DefaultConfig."""
     config = DefaultConfig()
     assert config is not None
+
+
+def test_default_config_attributes():
+    """Basic test for the class DefaultConfig."""
+    config = DefaultConfig()
+
+    # basic configuration check
+    attributes = ("ecosystem", "cve_age", "feed_dir", "feed_names", "date_range", "cve_id",
+                  "package_name", "cpe2pkg_path", "pkgfile_dir", "use_nvdtoolkit",
+                  "nvdtoolkit_export_dir")
+    for attribute in attributes:
+        assert hasattr(config, attribute)
 
 
 def test_runtime_config():
